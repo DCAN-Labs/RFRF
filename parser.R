0471abe24b0e873d6b65ef630eb2d6ac87fa4660
Joao Guassi Moreira <jguassimoreira@ucla.edu>
2019-08-22 15:19:46 -0700

    updating/saving work on parser function

--- parser.R
+++ parser.R
@@ -2,16 +2,25 @@
 #' @return Returns missing data info, unformatted data, user inputs, data type
 #' @keywords 
 
-read_in_dat = function(data_path,
-                       user_input) {
-  input.dat = readr::read_csv(data_path, col_names = FALSE, col_types = cols())
+read_in_dat = function(data.path,
+                       long.data = FALSE,
+                       outcome.var = NULL) {
+  input.dat = readr::read_csv(data.path, col_names = FALSE, col_types = cols())
+  
   if (is.character(input.dat[[1,1]])) {
     
         data.type = "df" #"df" for dataframe, "pm" for proximity matrix
-        format.dat = data.frame(input.dat[-1,])
+        format.dat = data.frame(input.dat[-1,])  
         names(format.dat) = input.dat[1,]
+        outcome.var = ifelse(is.null(outcome.var), 
+               names(format.dat)[1],
+               outcome.var)
         
-  } else if () {
+  } else if (is.numeric(input.dat[[1]])) {
+    
+        data.type = "pm"
+        format.dat = data.frame(input.dat)
+        names(format.dat) = paste("sub", sprintf("%04d", c(1:ncol(format.dat))),sep="")
     
   } else {
     
