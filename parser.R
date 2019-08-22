69b5ce19b89cde5a5741c208bd19512ea64cfdde
Joao Guassi Moreira <jguassimoreira@ucla.edu>
2019-08-22 11:43:40 -0700

    testing out a push

---
+++ parser.R
@@ -0,0 +1,30 @@
+#' Parse input data and return user params, datatype, unformatted data
+#' @return Returns missing data info, unformatted data, user inputs, data type
+#' @keywords 
+
+read_in_dat = function(data_path,
+                       user_input) {
+  input.dat = readr::read_csv(data_path, col_names = FALSE, col_types = cols())
+  if (is.character(input.dat[[1,1]])) {
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
