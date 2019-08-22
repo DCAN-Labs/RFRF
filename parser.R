0d9cd935ad1f0e80114e1e42d0c677701aaabaaf
Joao Guassi Moreira <jguassimoreira@ucla.edu>
2019-08-22 12:03:12 -0700

    updating/saving work on parser function

--- parser.R
+++ parser.R
@@ -7,6 +7,14 @@ read_in_dat = function(data_path,
   input.dat = readr::read_csv(data_path, col_names = FALSE, col_types = cols())
   if (is.character(input.dat[[1,1]])) {
     
+        data.type = "df" #"df" for dataframe, "pm" for proximity matrix
+        format.dat = data.frame(input.dat[-1,])
+        names(format.dat) = input.dat[1,]
+        
+  } else if () {
+    
+  } else {
+    
   }
   
   
