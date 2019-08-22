416f9644b177d6e371f32a4ecc5ec314e6044ff7
Joao Guassi Moreira <jguassimoreira@ucla.edu>
2019-08-22 15:48:14 -0700

    change file structure organization

--- parser/parser.R
+++ parser/parser.R
@@ -1,5 +1,5 @@
 #' Parse input data and return user params, datatype, unformatted data
-#' @return Returns missing data info, unformatted data, user inputs, data type
+#' @return Returns missing data info, unformatted data, user inputs, data type in a list
 #' @keywords 
 
 read_in_dat = function(data.path,
@@ -9,7 +9,7 @@ read_in_dat = function(data.path,
   
   if (is.character(input.dat[[1,1]])) {
     
-        data.type = "df" #"df" for dataframe, "pm" for proximity matrix
+        data.type = "ds" #"df" for dataset, "pm" for proximity matrix
         format.dat = data.frame(input.dat[-1,])  
         names(format.dat) = input.dat[1,]
         outcome.var = ifelse(is.null(outcome.var), 
