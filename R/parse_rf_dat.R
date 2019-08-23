7d1936447c41076affd73e98f3f3cf0ccc9b7b5a
Joao Guassi Moreira <jguassimoreira@ucla.edu>
2019-08-23 12:01:26 -0700

    updating parse_rf_dat function

--- R/parse_rf_dat.R
+++ R/parse_rf_dat.R
@@ -1,10 +1,14 @@
 #' Parse input data and return user params, datatype, unformatted data
 #' @return Returns missing data info, unformatted data, user inputs, data type in a list
 #' @keywords 
-parse.rf.dat = function(data.path,
+parse_rf_dat = function(data.path,
                        dat.type = c("ds", "pm"),
+                       rm.dat = c("rm", "cs")
                        outcome.var = NULL) {
   dat.type = match.arg(dat.type)
+  rm.dat = match.arg(rm.dat)
+  
+  ## read in data, validate, format
   if (dat.type == "ds") {
     
     input.dat = readr::read_csv(data.path, col_names = TRUE, col_types = cols())
@@ -17,6 +21,7 @@ parse.rf.dat = function(data.path,
   } else if (dat.type == "pm") {
     
     input.dat = readr::read_csv(data.path, col_names = FALSE, col_types = cols())
+    
     if (check_num_mat(input.dat)) {
       
       format.dat = data.frame(input.dat)
@@ -34,8 +39,21 @@ parse.rf.dat = function(data.path,
   }
 
   ## check for missing data
+  summary.table = skimr::skim(format.dat)
   
-  
+  if (dat.type == "ds") {
+    
+    
+    
+  } else { #dat.type = "pm
+    
+    if (sum(as.numeric(summary.table$formatted[summary.table$stat=="missing"])) != 0) {
+      
+      stop("halting program - missing data detected in proximity matrix")
+      
+    }
+    
+  }
   
   
   
