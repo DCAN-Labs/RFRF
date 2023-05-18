301fe7223cff13f0fbcb6945539ec0471ffe08ff
paul-reiners <paul.reiners@gmail.com>
2023-05-18 10:26:54 -0500

    Adding tests.

--- scripts/check_num_mat.R
+++ scripts/check_num_mat.R
@@ -5,11 +5,12 @@
 #' @param mat2check The matrix to check for solely numbers
 #'
 #' @return TRUE if and only if all the elements in the matrix are numeric
+#'
 #' @examples
 #' check_num_mat(matrix(c(3:14), nrow = 4, byrow = TRUE)
 check_num_mat = function(mat2check) {
   
-  ans = sum(sapply(as.matrix(mat2check), is.numeric))/length(sapply(as.matrix(mat2check), is.numeric))
+  ans = sum(sapply(as.matrix(mat2check), is.numeric)) / length(sapply(as.matrix(mat2check), is.numeric))
   out = ifelse(ans == 1, TRUE, FALSE)
   
   return(out)
---
+++ tests/test_check_num_mat.m
@@ -0,0 +1,3 @@
+check_num_mat_expected = 1
+
+testthat.expect_equal(check_num_mat_expected, [1 2 3 4])
