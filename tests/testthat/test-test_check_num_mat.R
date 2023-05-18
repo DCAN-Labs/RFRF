e522911c5a8d373381f2d95ca6cb7dd5fe3675bf
paul-reiners <paul.reiners@gmail.com>
2023-05-18 14:38:11 -0500

    Adding test files.

---
+++ tests/testthat/test-test_check_num_mat.R
@@ -0,0 +1,9 @@
+test_that("can determine if matrix contains only numbers", {
+  library(testthat)
+  source("../../R/check_num_mat.R")
+
+  M <- matrix(c(3:14), nrow = 4, byrow = TRUE)
+  actSolution = check_num_mat(M)
+  expSolution = TRUE
+  expect_equal(actSolution, expSolution)
+})
