8ddfd51b87b5e35f7541929b9413932f38d04d8c
ericfeczko <18431357+ericfeczko@users.noreply.github.com>
2023-05-25 12:35:30 -0500

    added expectations to test-RF_train.R

--- tests/testthat/test-RF_train.R
+++ tests/testthat/test-RF_train.R
@@ -8,6 +8,8 @@ test_that("RF_train", {
   covariance_matrix <- diag(2)
   outcome_column <- 1
   means <- c(0,0)
+  mtry <- 1
+  nodesize <- 1
 
   #create simulated data for the RF_train
   training_dataset <- simulate_data(number_of_training_participants,covariance_matrix,outcome_column,means)
@@ -18,8 +20,8 @@ test_that("RF_train", {
 
   #check the outputs (TODO: replaced by a search/these things equal, with usethis and testthat )
   expect_equal(random_forest$call)
-  expect_equal(random_forest$mtry)
-  expect_equal(random_forest$nodesize)
+  expect_equal(random_forest$mtry, 1)
+  expect_equal(random_forest$nodesize, 1)
   expect_equal(random_forest$yvar)
   expect_equal(random_forest$yvar_names)
   expect_equal(random_forest$xvar)
