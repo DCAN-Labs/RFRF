dd830af992781d229e7cebc303a72b7dfd3d95d2
Jacob Lundquist <lundq163@ad.ahc.umn.edu>
2023-06-16 16:55:55 -0500

    made changes to RF_test, RF_train, and assoicated test files

--- R/RF_train.R
+++ R/RF_train.R
@@ -1,7 +1,7 @@
 #' RF_train
 #'
 #' this function trains a random forest model with training data folds
-#' @param ntrain_folds
+#' @param ntrain_folds #is this parameter being used here?
 #' @param data
 #' @param formula
 #' @param mtry
@@ -15,10 +15,10 @@
 #' outcome_column <- 1
 #' means <- c(0,0)
 #' training_dataset <- simulate_data(number_of_training_participants,covariance_matrix,outcome_column,means)
-#' random_forest <- RF_Train(ntrain_folds=10,data=training_dataset,formula=y~x,mtry=NULL,nodesize=NULL,model_type="rfsrc")
+#' random_forest <- RF_Train(data=training_dataset,formula=y~x,mtry=NULL,nodesize=NULL,model_type="rfsrc")
 
 
-RF_train <- function(ntrain_folds=10,data=training_dataset,formula=y~x,mtry=NULL,nodesize=NULL,model_type="rfsrc"){
+RF_train <- function(data=training_dataset,formula=y~x,mtry=NULL,nodesize=NULL,model_type="rfsrc"){
   library(randomForestSRC)
   if (model_type=="rfsrc"){
     random_forest = rfsrc(formula=formula,data=data,mtry=mtry,nodesize=nodesize)
