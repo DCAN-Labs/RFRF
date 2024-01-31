007a577fa63461ee7f61ec1427ad5b6b63a5e130
lundq163 <lundq163@umn.edu>
2024-01-31 16:02:08 -0600

    updating subtype id unit test, not done yet though

--- tests/testthat/test-Subtype_ID.R
+++ tests/testthat/test-Subtype_ID.R
@@ -11,6 +11,10 @@ test_that("Subtype_ID", {
   covariance_matrix <- diag(2)
   outcome_column <- 1
   means <- c(0,0)
+  # declare the expected input variables for creating the test forest
+  formula = y~x
+  mtry <- 1
+  nodesize <- 1
   # declare the expected input variables for RF_prediction
   proximity <- array()
   yvar <- data.frame # "numeric", is.matrix(yvar)
@@ -20,7 +24,13 @@ test_that("Subtype_ID", {
   err.rate <- float
 
   # create simulated dataset for RF_prediction
+  training_dataset <- simulate_data(number_of_training_participants,covariance_matrix,outcome_column,means)
   testing_dataset <- simulate_data(number_of_testing_participants,covariance_matrix,outcome_column,means)
+
+  #create test forest
+  test_random_forest <- RF_train(training_dataset,formula,mtry,nodesize)
+
+  #determine RF_prediction values
 
   # create matrix_to_graph
   matrix_to_graph <- RF_Prediction(proximity, yvar, yvar.names, xvar, xvar.names, err.rate, testing_dataset) # does testing_dataset need to be included here, or is it satisfied with xvar and yvar?
