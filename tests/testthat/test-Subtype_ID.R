b479c6a0f0c6a165004388355c766cb13d201cc7
lundq163 <lundq163@umn.edu>
2024-02-04 14:14:05 -0600

    created Subtype_ID function

--- tests/testthat/test-Subtype_ID.R
+++ tests/testthat/test-Subtype_ID.R
@@ -3,10 +3,8 @@
 test_that("Subtype_ID", {
   #loads neccessary environment, packages, and dependencies
   library(RFRF)
-
-  # do we need to combine RF_Optimal_Test and RF_Prediction?
-
   # declare the expected input variables for simulate_data
+  number_of_training_participants <- 10
   number_of_testing_participants <- 10
   covariance_matrix <- diag(2)
   outcome_column <- 1
@@ -15,13 +13,11 @@ test_that("Subtype_ID", {
   formula = y~x
   mtry <- 1
   nodesize <- 1
-  # declare the expected input variables for RF_prediction
-  proximity <- array()
-  yvar <- data.frame # "numeric", is.matrix(yvar)
-  yvar.names <- string # "character"
-  xvar <- data.frame # is.data.frame(xvar)
-  xvar.names <- string # "character"
-  err.rate <- float
+  proximity_flag = TRUE
+  # declare the expected input variables for Subtype_ID
+  mode = "undirected"
+  weighted = TRUE
+  diag = FALSE
 
   # create simulated dataset for RF_prediction
   training_dataset <- simulate_data(number_of_training_participants,covariance_matrix,outcome_column,means)
@@ -31,19 +27,13 @@ test_that("Subtype_ID", {
   test_random_forest <- RF_train(training_dataset,formula,mtry,nodesize)
 
   #determine RF_prediction values
+  RF_prediction_test <- RF_optimal_test(test_random_forest,testing_dataset,proximity_flag)
 
-  # create matrix_to_graph
-  matrix_to_graph <- RF_Prediction(proximity, yvar, yvar.names, xvar, xvar.names, err.rate, testing_dataset) # does testing_dataset need to be included here, or is it satisfied with xvar and yvar?
+  # create subtypes
+  subtypes <- Subtype_ID(proximity_matrix=RF_prediction_test$proximity, mode=mode, weighted=weighted, diag=diag)
 
-  # check the outputs of matrix_to_graph
-  # needs a lot more review
-  expect_true(matrix_to_graph$adjmatrix)
-  expect_true(matrix_to_graph$mode)
-  expect_true(matrix_to_graph$weighted)
-  expect_true(matrix_to_graph$diag)
-  #print(matrix_to_graph.graph_from_adjacency_mattrix())
-
-  # create proximity_graph
-
-  # check community detection function
+  # check the outputs of Subtype_ID
+  # need to build out expects
+  expect_type(subtypes$length, "integer")
+  expect_type(subtypes$sizes, "numeric")
 })
