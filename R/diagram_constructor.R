3c2ee43154cc193c11c23eda53b76e7d7154a26b
lundq163 <lundq163@umn.edu>
2024-02-11 13:27:20 -0600

    edits to diagram constructor

--- R/diagram_constructor.R
+++ R/diagram_constructor.R
@@ -15,28 +15,28 @@ flow_diagram="#stroke: #000000
 #.proxmat: #fill=#FF007F visual=ellipse
 #.subtype: #fill=#00FF00 visual=ellipse
 [ <frame>RFRF flow diagram|
-[<fullbox>Full Data] --:> [Holdout]
-[Holdout] --:> [<trainbox>Train Data]
-[Holdout] --:> [<testbox>Test Data]
-[<trainbox>Train Data] --:> [cross-validation]
-[cross-validation] cv folds --:> [<trainbox>Train Folds]
-[cross-validation] --:> [<testbox>Valid Fold]
-[Train Folds] tuning --:> [RF train]
-[RF train] selection --:> [RF test]
-[<testbox>Valid Fold] testing --:> [RF test]
-[RF test] --:> [RF model]
-[<trainbox>Train Data] train --:> [RF model]
-[<testbox>Test Data] test --:> [RF model]
-[RF model] --:> [<proxmat>proximity matrix]
-[<proxmat>proximity matrix] --:> [subtype identification]
-[subtype identification] --:> [<subtype>subtypes]
+[<fullbox>Full Data] --:> [holdout]
+[holdout] --:> [<trainbox>Train Data]
+[holdout] --:> [<testbox>Test Data]
+[<trainbox>Train Data] --:> [cross_validation]
+[cross_validation] cv folds --:> [<trainbox>Train Folds]
+[cross_validation] --:> [<testbox>Valid Fold]
+[Train Folds] tuning --:> [RF_train]
+[RF_train] selection --:> [RF_test]
+[<testbox>Valid Fold] testing --:> [RF_test]
+[RF_test] --:> [RF_model]
+[<trainbox>Train Data] train --:> [RF_model]
+[<testbox>Test Data] test --:> [RF_model]
+[RF_model] RF_prediction --:> [<proxmat>proximity_matrix]
+[<proxmat>proximity_matrix] --:> [Subtype_ID]
+[Subtype_ID] --:> [<subtype>subtypes]
 ]"
 
 #UML diagrams for RFRF
 
-#Holdout diagram
-#holdout takes in full datasets and produces a stratified holdout dataset and a train dataset
-Holdout_UML_diagram="#stroke: #000000
+#holdout diagram
+#holdout takes in full datasets and produces a holdout test dataset and a train dataset
+holdout_diagram="#stroke: #000000
 #direction: down
 #padding: 4
 #spacing: 20
@@ -44,37 +44,38 @@ Holdout_UML_diagram="#stroke: #000000
 #gutter: 20
 #.databox: #fill=#00FFFF
 #.trainbox: #fill=#FFFF00
-[<frame> Holdout UML Diagram|
-[<databox>Input Data| file type: string
-file name: string
-file path: string]
-[<databox>Input Data] -:> [Read File| open()
+[<frame> holdout diagram|
+[<databox>Input Data] -:> [read_file| file_type: string
+file_name: string
+file_path: string | open()
 close()]
-[Read File] -:> [<databox>Full Data| data: data.frame
-ID column: string
-Outcome Column: string
+[read_file] -:> [<databox>Full Data| data: data.frame
+ID_column: string
+outcome_column: string
 nrows: int
 ncols: int]
-[<databox>Full Data] -:> [Create Data Holdout| holdout fraction: 0-1
-stratify by: string
-even stratification: boolean | split_data()
+[<databox>Full Data] -:> [holdout| data: Full Data
+seed: int
+groups: string
+holdout_fraction: 0-1
+even_stratification: boolean | randomize()
 stratify_data()
-randomize()]
-[Create Data Holdout] -:> [<trainbox>Train Data|  data: data.frame
-ID column: string
-Outcome Column: string
+split_data()]
+[holdout] -:> [<trainbox>Train Data|  data: data.frame
+ID_column: string
+outcome_column: string
 nrows: int
 ncols: int]
-[Create Data Holdout] -:> [<databox>Test Data|  data: data.frame
-ID column: string
-Outcome Column: string
+[holdout] -:> [<databox>Test Data|  data: data.frame
+ID_column: string
+outcome_column: string
 nrows: int
 ncols: int]
 ]"
 
-#cross-validation diagram -- cross validation tak
-#cross validation takes in a training dataset and produces stratified fold to send through RF_train and RF_test
-CV_UML_diagram="#stroke: #000000
+#cross_validation diagram
+#cross validation takes in a training dataset and produces stratified folds to send through RF_train and RF_test
+cross_validation_diagram="#stroke: #000000
 #direction: down
 #padding: 4
 #spacing: 20
@@ -82,21 +83,23 @@ CV_UML_diagram="#stroke: #000000
 #gutter: 20
 #.databox: #fill=#FFFF00
 #.testbox: #fill=#00FFFF
-[<frame> Cross-Validation UML Diagram|
+[<frame> cross_validation diagram|
 [<databox>Train Data| data: data.frame
-ID column: string
-Outcome Column: string
+ID_column: string
+outcome_column: string
 nrows: int
 ncols: int]
-[<databox>Train Data] -:> [Cross Validation Folds| nfolds: int
-stratify by: string
-even stratification: boolean | split_data()
+[<databox>Train Data] -:> [cross_validation| data: Train Data
+seed: int
+nfolds: int
+groups: string
+even_stratification: boolean | randomize()
 stratify_data()
-randomize()]
-[Cross Validation Folds] -:> [<databox>Output Folds|  nfolds: int
+split_data()]
+[cross_validation] -:> [<databox>Train Data Folds|  nfolds: int
 data: data.frame
-ID column: string
-Outcome Column: string
+ID_column: string
+outcome_column: string
 nrows: int
 ncols: int]
 ]"
@@ -112,27 +115,26 @@ RF_train_diagram="#stroke: #000000
 #gutter: 20
 #.databox: #fill=#FFFF00
 #.forestbox: #fill=#FF1111
-[<frame> RF train Diagram|
-[<databox>Train Folds| ntrain_folds: int
+[<frame> RF train diagram|
+[<databox>Train Data Folds| nfolds: int
 data: data.frame
-ID column: string
-Outcome Column: string
+ID_column: string
+outcome_column: string
 nrows: int
 ncols: int]
-[Model Select | hyperparameter: string | model_choice()]
-[<databox>Train Folds] -:> [RF train | Model Select
-ntrain_folds: int
-data: data.frame
+[model_select | hyperparameter: string | model_choice()]
+[<databox>Train Data Folds] -:> [RF_train | data: Train Data Fold
 formula: string
 mtry: int
 nodesize: int
-tune.rfsrc() | rfsrc()
+tune.rfsrc()
+model_select() | rfsrc()
 rfsrc.fast()
 sidClustering.rfsrc()
 imbalanced.rfsrc()
 quantreg.rfsrc()]
-[Model Select] -:> [RF train]
-[RF train] -:> [<forestbox> Random Forest | call: string
+[model_select] -:> [RF_train]
+[RF_train] -:> [<forestbox> random_forest | call: string
 mtry: int
 nodesize: int
 yvar: data.frame
@@ -153,8 +155,8 @@ RF_test_diagram="#stroke: #000000
 #.forestbox: #fill=#FF1111
 #.testbox: #fill=#00FFFF
 #.optimalbox: #fill=#FF7777
-[<frame> RF Test Diagram|
-[<forestbox> Random Forest | call: string
+[<frame> RF_test diagram|
+[<forestbox> random_forest | call: string
 mtry: int
 nodesize: int
 yvar: data.frame
@@ -162,25 +164,29 @@ yvar.names: string
 xvar: data.frame
 xvar.names: string
 forest: forest ]
-[<testbox>Test Fold|  data: data.frame
-ID column: string
-Outcome Column: string
+[<testbox>Test Data Fold|  data: data.frame
+ID_column: string
+outcome_column: string
 nrows: int
 ncols: int]
-[<forestbox> Random Forest] -:> [RF_test | hyperparameter: string
+[<forestbox> random_forest] -:> [RF_test | object: random_forest
+data: Test Data Fold
+proximity: boolean
+hyperparameter: string
 file.name: string
 | predict.rfsrc()
 write.tsv()
 get_auc()]
 [<testbox>Test Fold] -:> [RF_test]
-[RF_test] -:> [<optimalbox>Optimal RF Parameters | call: string
+[RF_test] -:> [<optimalbox>optimal_RF_parameters | call: string
 mtry: int
 nodesize: int
-err.rate: float]
+err.rate: float
+proximity: array]
 ]"
 
 #RF model diagram
-#RF model takes in the training data and trains an RF using the
+#RF model takes in the training data and trains an RF with the optimal RF hyperparameters
 RF_model_diagram="#stroke: #000000
 #direction: down
 #padding: 4
@@ -194,17 +200,18 @@ RF_model_diagram="#stroke: #000000
 #.forestbox: #fill=#FF1111
 #.trainbox: #fill=#FFFF00
 #.predictbox: #fill=#FF007F
-[<frame> RF Model Diagram|
-[<optimalbox>Optimal RF Parameters | call: string
+[<frame> RF_model_diagram|
+[<optimalbox>optimal_RF_parameters | call: string
 mtry: int
 nodesize: int
-err.rate: float]
+err.rate: float
+proximity: array]
 [<trainbox>Train Data|  data: data.frame
-ID column: string
-Outcome Column: string
+ID_column: string
+outcome_column: string
 nrows: int
 ncols: int]
-[<optimalbox>Optimal RF Parameters] -:> [RF Optimal Train | data: data.frame
+[<optimalbox>optimal_RF_parameters] -:> [RF_optimal_train | data: Train Data
 formula: string
 file.path: string
 file.name: string | read.tsv()
@@ -213,8 +220,8 @@ rfsrc.fast()
 sidClustering.rfsrc()
 imbalanced.rfsrc()
 quantreg.rfsrc()]
-[<trainbox>Train Data] -:> [RF Optimal Train]
-[RF Optimal Train] -:> [<forestbox> Optimal Random Forest | call: string
+[<trainbox>Train Data] -:> [RF_optimal_train]
+[RF_optimal_train] -:> [<forestbox> optimal_random_forest | call: string
 mtry: int
 nodesize: int
 yvar: data.frame
@@ -222,57 +229,51 @@ yvar.names: string
 xvar: data.frame
 xvar.names: string
 forest: forest ]
-[<forestbox> Optimal Random Forest] -:> [RF Optimal Test | hyperparameter: string
+[<forestbox> optimal_random_forest] -:> [RF_optimal_test | data: Test Data Fold
+proximity: boolean
+hyperparameter: string
 file.name: string |
 predict.rfsrc()
-create.matrix()]
+get.auc()
+write.tsv()]
 [<testbox>Test Data| data: data.frame
-ID column: string
-Outcome Column: string
+ID_column: string
+outcome_column: string
 nrows: int
 ncols: int]
-[<testbox>Test Data] -:> [RF Optimal Test]
-[RF Optimal Test] -:> [<predictbox> RF Prediction | proximity: array
+[<testbox>Test Data] -:> [RF_optimal_test]
+[RF_optimal_test] -:> [<predictbox> RF_prediction | proximity: array
 yvar: data.frame
 yvar.names: string
 xvar: data.frame
 xvar.names: string
-err.rate: float |
-get.auc()]
+err.rate: float]
 ]"
 
-#subtype ID diagram for RFRF
-subtype_ID_diagram="#stroke: #000000
+#Subtype ID diagram for RFRF
+Subtype_ID_diagram="#stroke: #000000
 #direction: down
 #padding: 4
 #spacing: 20
 #fontsize: 10
 #gutter: 20
 #.testbox: #fill=#00FFFF
-[<frame>Subtype ID Diagram|
-[RF Prediction | proximity: array
+#.subtype: #fill=#00FF00 visual=ellipse
+[<frame>Subtype_ID diagram|
+[RF_prediction | proximity: array
 yvar: data.frame
 yvar.names: string
 xvar: data.frame
 xvar.names: string
-err.rate: float |
-get.auc()]
-[RF Prediction] -:> [matrix to graph |
-adjmatrix: array
-mode: undirected
-weighted: TRUE
-diag: FALSE |
-graph_from_adjacency_matrix()]
-[<testbox>Test Data| data: data.frame
-ID column: string
-Outcome Column: string
-nrows: int
-ncols: int]
-[<testbox>Test Data] -:> [matrix to graph]
-[matrix to graph] -:> [proximity graph |
+err.rate: float ]
+[RF_prediction] -:> [graph_from_adjacency_matrix |
+proximity_matrix: proximity
+mode: string
+weighted: boolean
+diag: boolean]
+[graph_from_adjacency_matrix] -:> [cluster_infomap |
 proximity_graph: graph]
-[proximity graph] -:> [community detection function |
-cluster_infomap()]
+[cluster_infomap] -:> [<subtype>subtypes]
 ]"
 
 #interface diagram for RFRF
@@ -290,12 +291,12 @@ interface_diagram="#stroke: #000000
 
 #diagram visualization (saving may not work properly within RStudio)
 nomnoml(code = flow_diagram,png='flow_diagram.png')
-nomnoml(code = Holdout_UML_diagram,png='holdout_UML_diagram.png')
-nomnoml(code = CV_UML_diagram,png='CV_UML_diagram.png')
+nomnoml(code = holdout_diagram,png='holdout_diagram.png')
+nomnoml(code = cross_validation_diagram,png='cross_validation_diagram.png')
 nomnoml(code = RF_train_diagram,png='RF_train_diagram.png')
 nomnoml(code = RF_test_diagram,png='RF_test_diagram.png')
 nomnoml(code = RF_model_diagram,png='RF_model_diagram.png')
-nomnoml(code = subtype_ID_diagram,png='subtype_ID_diagram.png')
+nomnoml(code = Subtype_ID_diagram,png='Subtype_ID_diagram.png')
 
 
 
